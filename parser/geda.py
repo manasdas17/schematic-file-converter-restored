5a732802d1c391f79be6f48d488d267c0bc1ce02
Sebastian Vetter <sebastian@roadside-developer.com>
2011-12-13 16:39:42 -0700

    added object indentifiers to parser

--- parser/geda.py
+++ parser/geda.py
@@ -1,13 +1,43 @@
 #!/usr/bin/env python
-#
-# Basic Strategy
-# 0) ...
+""" The gEDA schematics are based on a grid in MILS (1/1000 of an inch).
+"""
 
 from core.design import Design
 
 
 class GEDA:
     """ The GEDA Format Parser """
+
+    DELIMITER = ' '
+
+    OBJECT_TYPES = {
+        'v': None, # gEDA version
+        'L': None, # line
+        'G': None, # box
+        'B': None, # circle
+        'A': None, # arc
+        'T': None, # text
+        'N': None, # net segment
+        'U': None, # bus
+        'P': None, # pin (in sym)
+        'C': None, # component
+        'H': None, # SVG-like path
+    }
+
+    PATH_INSTRUCTION = {
+        ## path related types
+        'M': None, # moveto (absolute)
+        'm': None, # moveto (relative)
+
+        'L': None, # lineto (absolute)
+        'l': None, # lineto (relative)
+
+        'C': None, # curveto (absolute)
+        'c': None, # curveto (relative)
+
+        'Z': None, # closepath
+        'z': None, # closepath
+    }
 
     def __init__(self):
         pass
@@ -16,6 +46,16 @@ class GEDA:
         """ Parse a gEDA file into a design """
         design = Design()
         f = open(filename, "w")
-        #TODO: Read!
+
         f.close()
         return design
+
+    def parse_object(self, stream):
+        line = stream.readline().split(self.DELIMITER)
+
+        if line[0] not in self.
+        print line
+         
+
+    def _parse_line(startx, starty, endx, endy, color, width, capstyle, dashstyle, dashlength, dashspace):
+        pass
