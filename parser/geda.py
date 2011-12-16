196ec01f4f4dccb1b40113f695ece24fc73bc4f3
Sebastian Vetter <sebastian@roadside-developer.com>
2011-12-15 17:34:49 -0700

    implemented parsers for primitive types and nets

--- parser/geda.py
+++ parser/geda.py
@@ -5,6 +5,8 @@
 import os
 
 from core import shape
+from core import components
+from core import net
 
 from core.design import Design
 from core.annotation import Annotation
@@ -19,37 +21,35 @@ class GEDA:
 
     DELIMITER = ' '
     SCALE_FACTOR = 10 #maps 100 MILS to 10 pixels
-
-    OBJECT_TYPES = {
-        'v': None, # gEDA version
-        'C': None, # component
-        'N': None, # net segment
-        'U': None, # bus (only graphical aid, not a component)
-        # text or attribute (context)
-        'T': None, 
-
-        'P': None, # pin (in sym)
-        'L': None, # line
-        'G': None, # box
-        'B': None, # circle
-        'A': None, # arc
-        'H': None, # SVG-like path
-    }
-
-    PATH_INSTRUCTION = {
+    OBJECT_TYPES = set([ 
+        'v', # gEDA version
+        'C', # component
+        'N', # net segment
+        'U', # bus (only graphical aid, not a component)
+        'T', # text or attribute (context)
+        'P', # pin (in sym)
+        'L', # line
+        'B', # box
+        'V', # circle
+        'A', # arc
+        'H', # SVG-like path
+        'H', # SVG-like path
         ## path related types
-        'M': None, # moveto (absolute)
-        'm': None, # moveto (relative)
-
-        'L': None, # lineto (absolute)
-        'l': None, # lineto (relative)
-
-        'C': None, # curveto (absolute)
-        'c': None, # curveto (relative)
-
-        'Z': None, # closepath
-        'z': None, # closepath
-    }
+        'M', # moveto (absolute)
+        'm', # moveto (relative)
+        'L', # lineto (absolute)
+        'l', # lineto (relative)
+        'C', # curveto (absolute)
+        'c', # curveto (relative)
+        'Z', # closepath
+        'z', # closepath
+        ## environments
+        '{', # attributes
+        '[', # embedded component
+        ## valid types but are ignored
+        'F', #font
+        'G', #picture
+    ])
 
     def __init__(self, symbol_dirs=None):
         """ Constuct a gEDA parser object. Specifying a list of symbol 
@@ -71,6 +71,8 @@ class GEDA:
         self.symbol_lookup = {}
 
         self.design = None 
+        self.segments = None
+        self.net_points = None
 
         for symbol_dir in symbol_dirs:
             if os.path.exists(symbol_dir):
@@ -87,67 +89,92 @@ format!"""
         """ Parse a gEDA file into a design """
 
         self.design = Design()
+
         self.segments = set()
+        self.net_point = dict()
+        self.net_names = dict()
 
         fh = open(filename, "r")
 
-        version_line = fh.readline().split(self.DELIMITER)
-        if version_line[0] != 'v':
+        typ, params = self.parse_element(fh)
+        if typ != 'v':
             raise GEDAParserError("cannot convert file, not in gEDA format")
 
-
+        ##TODO(elbaschid): handle parsing of the input file
 
         fh.close()
         return design
 
-    def parse_object(self, stream):
-        params = stream.readline().split(self.DELIMITER)
-        obj_type = params.pop(0)
+    def parse_stream(self, stream):
+        obj_type, params = self.parse_element(stream)
 
-        if obj_type not in self.OBJECT_TYPES:
-            raise GEDAParserError("unknown type '%s' in file", obj_type)
+        while obj_type != None:
 
-        if obj_type == 'T':
-            ##FIXME(elbaschid): if text has key=value form -> design level attribute
+            if obj_type == 'T':
+                ##FIXME(elbaschid): if text has key=value form -> design level attribute
 
-            ##Convert regular text into annotation
-            annotation = self.parse_text(stream, *params)
+                ##Convert regular text into annotation
+                annotation = self.parse_text(stream, *params)
 
-        elif obj_type == 'G':
-            ##(elbaschid): pictures are not supported in upverter
-            print "WARNING: ignoring picture in gEDA file. Not supported!"
-        elif obj_type == 'C':
-            ##TODO(elbaschid): check if sym file is embedded or not 
-            ##TODO(elbaschid): if EMBEDDED check for [] holding embedded definition
-            ##TODO(elbaschid): check for optional attribute definition in {}
-            raise NotImplementedError()
-        elif obj_type == 'N':
-            self.parse_segment(*params)
-            ##TODO(elbaschid): process net segment into NET
-            ##TODO(elbaschid): check for optional attribute definition in {}
-            raise NotImplementedError()
-        elif obj_type == 'U':
-            ## bus (only graphical feature NOT component)
-            ##TODO(elbaschid): process bus into NET
-            ##TODO(elbaschid): check for optional attribute definition in {}
-            raise NotImplementedError()
+            elif obj_type in ['G', 'F'] :
+                ## picture/font types are not supported in upverter
+                print "WARNING: ignoring picture/font in gEDA file. Not supported!"
+            elif obj_type == 'C':
+                ##TODO(elbaschid): check if sym file is embedded or not 
+                ##TODO(elbaschid): if EMBEDDED check for [] holding embedded definition
+                ##TODO(elbaschid): check for optional attribute definition in {}
+                raise NotImplementedError()
+            elif obj_type == 'N':
+                self.parse_segment(stream, *params)
+                ##TODO(elbaschid): process net segment into NET
+                ##TODO(elbaschid): check for optional attribute definition in {}
+            elif obj_type == 'U':
+                ## bus (only graphical feature NOT component)
+                ##TODO(elbaschid): process bus into NET
+                ##TODO(elbaschid): check for optional attribute definition in {}
+                raise NotImplementedError()
 
-        ## object types of different environemnts
-        elif obj_type == '{':
-            ##Attribute lists
-            raise NotImplementedError()
-        elif obj_type == '[':
-            ##embedded component
-            raise NotImplementedError()
+            ## object types of different environemnts
+            elif obj_type == '{':
+                ##Attribute lists
+                raise NotImplementedError()
+            elif obj_type == '[':
+                ##embedded component
+                raise NotImplementedError()
+
+            obj_type, params = self.parse_element(stream)
+
+    def divide_segments(self):
+        ## check if segments need to be divided
+        add_segs = set()
+        rem_segs = set()
+        for segment in self.segments:
+            for point in self.net_points.values():
+                if self.intersects_segment(segment, point):
+                    pt_a, pt_b = segment
+                    rem_segs.add(segment)
+                    add_segs.add((pt_a, point))
+                    add_segs.add((point, pt_b))
+
+        self.segments -= rem_segs
+        self.segments |= add_segs
     
     def parse_text(self, stream, x, y, color, size, visiblity, 
-                     show_name_value, angle, alignment, num_lines):
-
+                    show_name_value, angle, alignment, num_lines):
+        """ Parses text element and determins if text is a text object
+            or an attribute. 
+            Returns a tuple (key, value) for attribute and (None, Annotation())
+                otherwise.
+        """
         text = []
         for idx in range(int(num_lines)):
             text.append(stream.readline())
 
         text_str = ''.join(text)
+
+        if num_lines == 1 and '=' in text_str:
+            key, value = text_str.split('=', 1)
+            return key.strip(), value.strip()
 
         return Annotation(
             text_str,
@@ -157,8 +184,31 @@ format!"""
             self.conv_bool(visiblity),
         )
 
-    def parse_environment(self):
-        raise NotImplementedError()
+    def parse_environment(self, stream):
+        """ Checks if attribute environment starts in the next line
+            (marked by '{'). Environment only contains text elements
+            interpreted as text. 
+            Returns a dictionary of attributes.
+        """
+        current_pos = stream.tell()
+        typ, params = self.parse_element(stream)
+
+        #go back to previous position when no environment in stream
+        if typ != '{':
+            stream.seek(current_pos)
+            return None 
+
+        typ, params = self.parse_element(stream)
+
+        attributes = {}
+        while typ != '}':
+            if typ == 'T':
+                key, value = self.parse_text(stream, *params)
+                attributes[key] = value
+
+            typ, params = self.parse_element(stream)
+
+        return attributes
 
     def parse_embedded_component(self):
         raise NotImplementedError()
@@ -166,8 +216,66 @@ format!"""
     def parse_component(self):
         raise NotImplementedError()
 
-    def parse_segement(self):
-        raise NotImplementedError()
+    def store_netpoint(self, x, y):
+        if (x, y) not in self.net_points:
+            self.net_points[(x, y)] = net.NetPoint('%da%d' % (x, y), x, y)
+        return self.net_points[(x, y)]
+
+    def intersects_segment(self, segment, pt_c):
+        pt_a, pt_b = segment
+
+        #check vertical segment
+        if pt_a.x == pt_b.x == pt_c.x:
+            if min(pt_a.y, pt_b.y) < pt_c.y < max(pt_a.y, pt_b.y):
+                return True
+        #check vertical segment
+        elif pt_a.y == pt_b.y == pt_c.y:
+            if min(pt_a.x, pt_b.x) < pt_c.x < max(pt_a.x, pt_b.x):
+                return True
+        ## point C not on segment
+        return False
+
+    def parse_segment(self, stream, x1, y1, x2, y2, color):
+        #TODO(elbaschid): store segement for processing later
+        x1, y1 = self.conv_coords(x1, y1)
+        x2, y2 = self.conv_coords(x2, y2)
+
+        ## store segment points in global point list
+        pt_a = self.store_netpoint(x1, y1)
+        pt_b = self.store_netpoint(x2, y2)
+
+        ## add segment to global list for later processing
+        self.segments.add((pt_a, pt_b))
+
+        #attributes = self.parse_environment(stream)
+        #if attributes is not None:
+        #    #TODO(elbaschid): create net with name in attributes 
+        #    pass
+
+    def calculate_nets(self, segments):
+        nets = []
+
+        # Iterate over the segments, removing segments when added to a net
+        while segments:
+            seg = segments.pop() # pick a point
+            new_net = net.Net('')
+            new_net.connect(seg)
+            found = True
+
+            while found:
+                found = set()
+
+                for seg in segments: # iterate over segments
+                    if new_net.connected(seg): # segment touching the net
+                        new_net.connect(seg) # add the segment
+                        found.add(seg)
+
+                for seg in found:
+                    segments.remove(seg)
+
+            nets.append(new_net)
+
+        return nets
 
     def parse_bus(self):
         raise NotImplementedError()
@@ -175,7 +283,7 @@ format!"""
     def parse_path(self):
         raise NotImplementedError()
 
-    def parse_arc(self, center_x, center_y, radius, start_angle, sweep_angle, *args):
+    def parse_arc(self, center_x, center_y, radius, start_angle, sweep_angle, *style_args):
         return shape.Arc(
             self.conv_mils(center_x),
             self.conv_mils(center_y),
@@ -183,26 +291,78 @@ format!"""
             self.conv_angle(start_angle + sweep_angle),
             self.conv_mils(radius),
         )
-        raise NotImplementedError()
 
-    def parse_line(self):
-        raise NotImplementedError()
+    def parse_line(self, start_x, start_y, end_x, end_y, *style_args):
+        return shape.Line(
+            self.conv_coords(start_x, start_y),
+            self.conv_coords(end_x, end_y),
+        )
 
-    def parse_box(self):
-        raise NotImplementedError()
+    def parse_box(self, x, y, width, height, *style_args):
+        """ Creates rectangle from gEDA box with origin in bottom left
+            corner. All style related values are ignored.
+        """
+        return shape.Rectangle(
+            self.conv_mils(x+height),
+            self.conv_mils(y),
+            self.conv_mils(width),
+            self.conv_mils(height)
+        )
 
-    def parse_circle(self):
-        raise NotImplementedError()
+    def parse_circle(self, x, y, radius, *style_args):
+        return shape.Circle(
+            self.conv_mils(x),
+            self.conv_mils(y),
+            self.conv_mils(radius),
+        )
 
-    def parse_pin(self):
-        raise NotImplementedError()
+    def parse_pin(self, stream, x1, y1, x2, y2, color, pintype, whichend):
+
+        ## pin requires an attribute enviroment, so parse it first
+        attributes = self.parse_environment(stream)
+        
+        if attributes is None:
+            raise GEDAParserError('mandatory pin attributes missing')
+
+        if 'pinnumber' not in attributes:
+            raise GEDAParserError(
+                "mandatory attribute 'pinnumber' not assigned to pin"
+            )
+
+        ## determine wich end of the pin is the connected end
+        ## 0: first point is connector
+        ## 1: second point is connector
+        if whichend == 0:
+            connect_end = self.conv_coords(x1, y1) 
+            null_end = self.conv_coords(x2, y2) 
+        else:
+            null_end = self.conv_coords(x1, y1) 
+            connect_end = self.conv_coords(x2, y2) 
+
+        return components.Pin(
+            attributes['pinnumber'], #pin number
+            null_end,
+            connect_end,
+            label=attributes.get('pinlabel', None),
+        )
 
     def parse_element(self, stream):
-        element_items = stream.readline().split(self.DELIMITER)
-        object_type = element_items[0]
-        params = [int(x) for x in element_items[1:]]
+        element_items = stream.readline().strip().split(self.DELIMITER)
 
-        ##TODO(elbaschid): check for valid object types
+        if len(element_items[0]) == 0:
+            return None, []
+
+        object_type = element_items[0].strip()
+
+        if object_type == 'C':
+            ## last parameter is string, do not convert
+            params = [int(x) for x in element_items[1:-1]]
+            params.append(element_items[-1])
+        else:
+            params = [int(x) for x in element_items[1:]]
+
+        if object_type not in self.OBJECT_TYPES:
+            raise GEDAParserError("unknown type '%s' in file" % object_type)
         
         return object_type, params 
 
@@ -232,7 +392,7 @@ format!"""
         """
         if value in ['true', 'false']:
             return value
-        return str(bool(value) == True).lower()
+        return str(bool(int(value)) == True).lower()
 
     def conv_angle(self, angle):
         """ Converts *angle* (in degrees) to pi radians."""
--- test/geda/test_geda_parser.py
+++ test/geda/test_geda_parser.py
@@ -12,6 +12,8 @@ class TestGEDA(unittest.TestCase):
 
     def setUp(self):
         self.geda_parser = GEDA()
+        ## for easier validation 
+        self.geda_parser.SCALE_FACTOR = 10
 
     def test_constructor(self):
         geda_parser = GEDA()
@@ -42,7 +44,7 @@ class TestGEDA(unittest.TestCase):
 Text string!"""
 
         text_stream = StringIO.StringIO(valid_text)
-        params = text_stream.readline().split(' ')[1:]
+        typ, params =  self.geda_parser.parse_element(text_stream)
         annotation = self.geda_parser.parse_text(text_stream, *params)
 
         self.assertEquals(annotation.value, "Text string!")
@@ -57,10 +59,9 @@ Text string!
 And more ...
 and more ...
 text!"""
-
         text_stream = StringIO.StringIO(valid_text)
-        params = text_stream.readline().split(' ')[1:]
-        annotation = self.geda_parser.convert_text(text_stream, *params)
+        typ, params =  self.geda_parser.parse_element(text_stream)
+        annotation = self.geda_parser.parse_text(text_stream, *params)
 
         text = """Text string!
 And more ...
@@ -109,22 +110,107 @@ text!"""
             )
 
     def test_parse_environment(self):
-        raise NotImplementedError()
+        no_env = "P 100 600 200 600 1 0 0"
+        stream = StringIO.StringIO(no_env)
+        attributes = self.geda_parser.parse_environment(stream)
+        self.assertEquals(attributes, None)
+        self.assertEquals(stream.tell(), 0)
 
-    def test_parse_embedded_component(self):
-        raise NotImplementedError()
+        valid_env = """{
+W 150 650 5
+T 150 650 5 8 1 1 0 6 1
+pinnumber=3
+T 150 650 5 8 0 1 0 6 1
+pinseq=3
+T 250 500 9 16 0 1 0 0 1
+pinlabel=+=?
+T 150 550 5 8 0 1 0 8 1
+_sometype=in
+}"""
+        expected_attributes = {
+            'pinnumber': '3',
+            'pinseq': '3',
+            'pinlabel': '+=?',
+            '_sometype': 'in',
+        }
+        stream = StringIO.StringIO(valid_env)
+        attributes = self.geda_parser.parse_environment(stream)
 
-    def test_parse_component(self):
-        raise NotImplementedError()
+        self.assertEquals(attributes, expected_attributes)
 
-    def test_parse_segement(self):
-        raise NotImplementedError()
+    #def test_parse_embedded_component(self):
+    #    raise NotImplementedError()
 
-    def test_parse_bus(self):
-        raise NotImplementedError()
+    #def test_parse_component(self):
+    #    raise NotImplementedError()
 
-    def test_parse_path(self):
-        raise NotImplementedError()
+    def test_calculate_nets(self):
+        net_sample = """N 52100 44400 54300 44400 4
+N 53200 45100 53200 43500 4
+N 55000 44400 56600 44400 4
+N 55700 45100 55700 44400 4
+N 55700 44400 55700 43500 4"""
+
+        self.geda_parser.segments = set()
+        self.geda_parser.net_points = dict()
+
+        stream = StringIO.StringIO(net_sample)
+        self.geda_parser.parse_stream(stream)
+
+        self.geda_parser.divide_segments()
+
+        self.assertEquals(len(self.geda_parser.segments), 6)
+
+        nets = self.geda_parser.calculate_nets(self.geda_parser.segments)
+
+        self.assertEquals(len(nets), 3)
+
+
+    def test_parse_segment(self):
+        simple_segment = "N 47300 48500 43500 48500 4"
+
+        self.geda_parser.segments = set()
+        self.geda_parser.net_points = dict()
+
+        stream = StringIO.StringIO(simple_segment)
+        typ, params = self.geda_parser.parse_element(stream)
+        self.geda_parser.parse_segment(stream, *params)
+
+        segment = self.geda_parser.segments.pop()
+        self.assertEquals(segment, ((4730, 4850), (4350, 4850)))
+
+        expected_points = [(4730, 4850), (4350, 4850)]
+        for x, y in expected_points:
+            point = self.geda_parser.net_points[(x, y)]
+            self.assertEquals(point.point_id, '%da%d' % (x, y))
+            self.assertEquals(point.x, x)
+            self.assertEquals(point.y, y)
+
+
+        complex_segment = """N 47300 48500 43500 48500 4
+{
+T 43800 48300 5 10 1 1 0 0 1
+netname=+_1
+}"""
+        self.geda_parser.segments = set()
+        self.geda_parser.net_points = dict()
+
+        stream = StringIO.StringIO(complex_segment)
+        typ, params = self.geda_parser.parse_element(stream)
+        self.geda_parser.parse_segment(stream, *params)
+
+        expected_points = [(4730, 4850), (4350, 4850)]
+        for x, y in expected_points:
+            point = self.geda_parser.net_points[(x, y)]
+            self.assertEquals(point.point_id, '%da%d' % (x, y))
+            self.assertEquals(point.x, x)
+            self.assertEquals(point.y, y)
+
+    #def test_parse_bus(self):
+    #    raise NotImplementedError()
+
+    #def test_parse_path(self):
+    #    raise NotImplementedError()
 
     def test_parse_arc(self):
         typ, params =  self.geda_parser.parse_element(
@@ -162,16 +248,145 @@ text!"""
         self.assertEquals(arc_obj.end_angle, 0.3)
 
     def test_parse_line(self):
-        raise NotImplementedError()
+        test_strings = [
+            "L 40800 46600 45700 46600 3 0 0 0 -1 -1",
+            "L 42300 45900 42900 45500 3 0 0 0 -1 -1",
+            "L 44500 45000 44100 45200 3 0 0 0 -1 -1",
+        ]
+
+        for line_string in test_strings:
+            typ, params =  self.geda_parser.parse_element(
+                StringIO.StringIO(line_string)
+            )
+            line_obj = self.geda_parser.parse_line(*params)
+            self.assertEquals(line_obj.type, 'line')
+            self.assertEquals(
+                line_obj.p1.x, 
+                params[0]/self.geda_parser.SCALE_FACTOR
+            )
+            self.assertEquals(
+                line_obj.p1.y, 
+                params[1]/self.geda_parser.SCALE_FACTOR
+            )
+            self.assertEquals(
+                line_obj.p2.x, 
+                params[2]/self.geda_parser.SCALE_FACTOR
+            )
+            self.assertEquals(
+                line_obj.p2.y, 
+                params[3]/self.geda_parser.SCALE_FACTOR
+            )
 
     def test_parse_box(self):
-        raise NotImplementedError()
+        test_strings = [
+            "B 41700 42100 2900 1500 3 0 0 0 -1 -1 0 -1 -1 -1 -1 -1",
+            "B 46100 41100 1200 2600 3 0 0 0 -1 -1 0 -1 -1 -1 -1 -1",
+        ]
+
+        for rect_string in test_strings:
+            typ, params =  self.geda_parser.parse_element(
+                StringIO.StringIO(rect_string)
+            )
+            rect_obj = self.geda_parser.parse_box(*params)
+            self.assertEquals(rect_obj.type, 'rectangle')
+            self.assertEquals(
+                rect_obj.x, 
+                (params[0]+params[3])/self.geda_parser.SCALE_FACTOR
+            )
+            self.assertEquals(
+                rect_obj.y, 
+                params[1]/self.geda_parser.SCALE_FACTOR
+            )
+            self.assertEquals(
+                rect_obj.width, 
+                params[2]/self.geda_parser.SCALE_FACTOR
+            )
+            self.assertEquals(
+                rect_obj.height, 
+                params[3]/self.geda_parser.SCALE_FACTOR
+            )
+
 
     def test_parse_circle(self):
-        raise NotImplementedError()
+        test_strings = [
+            "V 49100 48800 900 3 0 0 0 -1 -1 0 -1 -1 -1 -1 -1",
+            "V 51200 49000 400 3 0 0 0 -1 -1 0 -1 -1 -1 -1 -1",
+        ]
+
+        for circle_string in test_strings:
+            typ, params =  self.geda_parser.parse_element(
+                StringIO.StringIO(circle_string)
+            )
+            circle_obj = self.geda_parser.parse_circle(*params)
+            self.assertEquals(circle_obj.type, 'circle')
+            self.assertEquals(
+                circle_obj.x, 
+                params[0]/self.geda_parser.SCALE_FACTOR
+            )
+            self.assertEquals(
+                circle_obj.y, 
+                params[1]/self.geda_parser.SCALE_FACTOR
+            )
+            self.assertEquals(
+                circle_obj.radius, 
+                params[2]/self.geda_parser.SCALE_FACTOR
+            )
 
     def test_parse_pin(self):
-        raise NotImplementedError()
+            stream = StringIO.StringIO('P 100 600 200 600 1 0 0\nM')
+            typ, params =  self.geda_parser.parse_element(stream)
+            self.assertRaises(
+                GEDAParserError,
+                self.geda_parser.parse_pin,
+                stream, 
+                *params
+            )
+
+            pin_sample = """P 100 600 200 600 1 0 0
+{
+T 150 650 5 8 1 1 0 6 1
+pinnumber=3
+T 150 650 5 8 0 1 0 6 1
+pinseq=3
+T 250 500 9 16 0 1 0 0 1
+pinlabel=+
+T 150 550 5 8 0 1 0 8 1
+pintype=in
+}"""
+            stream = StringIO.StringIO(pin_sample)
+            typ, params =  self.geda_parser.parse_element(stream)
+            pin = self.geda_parser.parse_pin(stream, *params)
+
+            self.assertEquals(pin.pin_number, '3')
+            self.assertEquals(pin.label, '+')
+            ## null_end
+            self.assertEquals(pin.p1.x, 20)
+            self.assertEquals(pin.p1.y, 60)
+            ## connect_end
+            self.assertEquals(pin.p2.x, 10)
+            self.assertEquals(pin.p2.y, 60)
+
+            reversed_pin_sample = """P 100 600 200 600 1 0 1
+{
+T 150 650 5 8 1 1 0 6 1
+pinnumber=E
+T 150 650 5 8 0 1 0 6 1
+pinseq=3
+T 150 550 5 8 0 1 0 8 1
+pintype=in
+}"""
+            stream = StringIO.StringIO(reversed_pin_sample)
+            typ, params =  self.geda_parser.parse_element(stream)
+            pin = self.geda_parser.parse_pin(stream, *params)
+
+            self.assertEquals(pin.pin_number, 'E')
+            self.assertEquals(pin.label, None) 
+            ## null_end
+            self.assertEquals(pin.p1.x, 10)
+            self.assertEquals(pin.p1.y, 60)
+            ## connect_end
+            self.assertEquals(pin.p2.x, 20)
+            self.assertEquals(pin.p2.y, 60)
 
     def test_parse_element(self):
         typ, params = self.geda_parser.parse_element(StringIO.StringIO('{'))
@@ -184,6 +399,14 @@ text!"""
         self.assertEquals(typ, 'A')
         self.assertEquals(params, [49, 34, 223])
         self.assertEquals(len(params), 3)
+
+        expected_params = [18600, 21500, 1, 0, 0, 'EMBEDDED555-1.sym']
+        string = " ".join(['C'] + [str(x) for x in expected_params])
+        typ, params = self.geda_parser.parse_element(
+            StringIO.StringIO(string)
+        )
+        self.assertEquals(typ, 'C')
+        self.assertEquals(params, expected_params)
 
 if __name__ == '__main__':
     unittest.main()
