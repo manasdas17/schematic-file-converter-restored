799dc66faf5e150b5a36284dfc706edf2e7c58e0
Dave Peticolas <dave@krondo.com>
2011-12-16 21:37:28 -0800

    Compound component support in kicad writer. More writer tests.

--- writer/kicad.py
+++ writer/kicad.py
@@ -54,7 +54,7 @@ class KiCAD(object):
 
     def write_libs(self, f, library_filename):
         """ Write the LIBS section of a kiCAD schematic """
-        f.write('LIBS:%s\n' % (splitext(library_filename)[0],))
+        f.write('LIBS:%s-cache\n' % (splitext(library_filename)[0],))
 
 
     def write_eelayer(self, f):
@@ -87,7 +87,7 @@ $EndDescr
         """ Write a $Comp component to a kiCAD schematic """
         f.write('$Comp\n')
         f.write('L %s %s\n' % (inst.library_id, inst.instance_id))
-        f.write('U 1 1 00000000\n')
+        f.write('U %d 1 00000000\n' % (inst.symbol_index,))
         f.write('P %d %d\n' % (inst.symbol_attributes[0].x,
                                inst.symbol_attributes[0].y))
         f.write('\t1    %d %d\n' % (inst.symbol_attributes[0].x,
@@ -142,55 +142,76 @@ $EndDescr
         f.write('#\n')
         f.write('# ' + cpt.name + '\n')
         f.write('#\n')
-        f.write('DEF %s %s 0 30 Y Y 1 F N\n' % (cpt.name, ref))
+        f.write('DEF %s %s 0 30 Y Y %d F N\n' %
+                (cpt.name, ref, len(cpt.symbols)))
         f.write('F0 "%s" 0 0 60 H V L CNN\n' % (ref,))
         f.write('F1 "%s" 0 60 60 H V L CNN\n' % (cpt.name,))
-        self.write_library_component_body(f, cpt.symbols[0].bodies[0])
+        self.write_symbols(f, cpt.symbols)
         f.write('ENDDEF\n')
 
 
-    def write_library_component_body(self, f, body):
+    def write_symbols(self, f, symbols):
         """ Write the DRAW portion (shapes and pins) of a kiCAD component """
         f.write('DRAW\n')
 
-        for shape in body.shapes:
-            self.write_shape(f, shape)
+        lines = {} # line template -> (set([units]), set([converts]))
 
-        for pin in body.pins:
-            self.write_pin(f, pin)
+        def add_line(line, unit, convert):
+            """ Add a line with a given unit and convert """
+            if line not in lines:
+                lines[line] = (set(), set())
+            lines[line][0].add(unit)
+            lines[line][1].add(convert)
+
+        for unit, symbol in enumerate(symbols, 1):
+            for body in symbol.bodies:
+                for shape in body.shapes:
+                    add_line(self.get_shape_line(shape), unit, 1)
+
+                for pin in body.pins:
+                    add_line(self.get_pin_line(pin), unit, 1)
+
+        for line, (units, converts) in lines.items():
+            if len(units) == len(symbols):
+                units = (0,)
+            if len(converts) == 2:
+                converts = (0,)
+            for unit in units:
+                for convert in converts:
+                    f.write(line % dict(unit=unit, convert=convert))
 
         f.write('ENDDRAW\n')
 
 
-    def write_shape(self, f, shape):
-        """ Write a Shape to a kiCAD cache library """
+    def get_shape_line(self, shape):
+        """ Return the line for a Shape in a kiCAD cache library """
         if shape.type == 'arc':
             # convert pi radians to tenths of degrees
             start = round(shape.start_angle * 1800)
             end = round(shape.end_angle * 1800)
-            f.write('A %d %d %d %d %d 0 1 0 N\n' %
+            return ('A %d %d %d %d %d %%(unit)d %%(convert)d 0 N\n' %
                     (shape.x, shape.y, shape.radius, start, end))
         elif shape.type == 'circle':
-            f.write('C %d %d %d 0 1 0 N\n' % (shape.x, shape.y, shape.radius))
+            return ('C %d %d %d %%(unit)d %%(convert)d 0 N\n' %
+                    (shape.x, shape.y, shape.radius))
         elif shape.type == 'polygon':
-            f.write('P %d 1 0 0 ' % (len(shape.points),))
-            for point in shape.points:
-                f.write('%d %d ' % (point.x, point.y))
-            f.write('N\n')
+            return ('P %d %%(unit)d %%(convert)d 0 %s N\n' %
+                    (len(shape.points),
+                     ' '.join('%d %d' % (p.x, p.y) for p in shape.points)))
         elif shape.type == 'rectangle':
-            f.write('S %d %d %d %d 0 1 0 N\n' %
+            return ('S %d %d %d %d %%(unit)d %%(convert)d 0 N\n' %
                     (shape.x, shape.y, shape.x + shape.width,
                      shape.y + shape.height))
         elif shape.type == 'label':
             angle = round(shape.rotation * 1800)
             align = shape.align[0].upper()
-            f.write('T %d %d %d 20 0 0 0 %s Normal 0 %s C\n' %
+            return ('T %d %d %d 20 0 %%(unit)d %%(convert)d %s Normal 0 %s C\n' %
                     (angle, shape.x, shape.y,
                      shape.text.replace(' ', '~'), align))
 
 
-    def write_pin(self, f, pin):
-        """ Write a Pin to a kiCAD cache library """
+    def get_pin_line(self, pin):
+        """ Return the line for a Pin in a kiCAD cache library """
         x, y = pin.p2.x, pin.p2.y
 
         if x == pin.p1.x: # vertical
@@ -206,7 +227,7 @@ $EndDescr
             else:
                 direction = 'R' # right
 
-        f.write('X ~ %s %d %d %d %s 60 60 1 1 B\n' %
+        return ('X ~ %s %d %d %d %s 60 60 %%(unit)d %%(convert)d B\n' %
                 (pin.pin_number, x, y, abs(length), direction))
 
 
--- writer/t/kicad_t.py
+++ writer/t/kicad_t.py
@@ -1,7 +1,9 @@
 # encoding: utf-8
 from writer.kicad import KiCAD
+from core.design import Design
 from core.components import Pin
 from core.net import Net, NetPoint
+from core.component_instance import ComponentInstance, SymbolAttribute
 import unittest
 
 from cStringIO import StringIO
@@ -11,6 +13,42 @@ class KiCADTests(unittest.TestCase):
 
     def test_create_new_kicad_writer(self):
         assert KiCAD() is not None
+
+    def test_write_header(self):
+        design = Design()
+        design.design_attributes.metadata.updated_timestamp = 0
+        writer = KiCAD()
+        buf = StringIO()
+        writer.write_header(buf, design)
+        self.assertEqual(buf.getvalue()[:40], 'EESchema Schematic File Version 2  date ')
+
+    def test_write_libs(self):
+        writer = KiCAD()
+        buf = StringIO()
+        writer.write_libs(buf, 'test.sch')
+        self.assertEqual(buf.getvalue(), 'LIBS:test-cache\n')
+
+    def test_write_eelayer(self):
+        writer = KiCAD()
+        buf = StringIO()
+        writer.write_eelayer(buf)
+        self.assertEqual(buf.getvalue(), 'EELAYER 25  0\nEELAYER END\n')
+
+    def test_write_instance(self):
+        inst = ComponentInstance('id', 'libid', 1)
+        inst.add_symbol_attribute(SymbolAttribute(3, 4, 0.5))
+        writer = KiCAD()
+        buf = StringIO()
+        writer.write_instance(buf, inst)
+        self.assertEqual(buf.getvalue(), '''\
+$Comp
+L libid id
+U 1 1 00000000
+P 3 4
+\t1    3 4
+\t0    1    1    0
+$EndComp
+''')
 
     def test_write_net(self):
         net = Net('')
@@ -38,28 +76,28 @@ class KiCADTests(unittest.TestCase):
         writer.write_footer(buf)
         self.assertEqual(buf.getvalue(), '$EndSCHEMATC\n')
 
-    def test_write_pin(self):
+    def test_get_pin_line(self):
         writer = KiCAD()
 
-        buf = StringIO()
         pin = Pin('1', (-300, 100), (-600, 100))
-        writer.write_pin(buf, pin)
-        self.assertEqual(buf.getvalue(), 'X ~ 1 -600 100 300 R 60 60 1 1 B\n')
+        line = writer.get_pin_line(pin)
+        self.assertEqual(
+            line, 'X ~ 1 -600 100 300 R 60 60 %(unit)d %(convert)d B\n')
 
-        buf = StringIO()
         pin = Pin('1', (300, 100), (600, 100))
-        writer.write_pin(buf, pin)
-        self.assertEqual(buf.getvalue(), 'X ~ 1 600 100 300 L 60 60 1 1 B\n')
+        line = writer.get_pin_line(pin)
+        self.assertEqual(
+            line, 'X ~ 1 600 100 300 L 60 60 %(unit)d %(convert)d B\n')
 
-        buf = StringIO()
         pin = Pin('2', (0, -1300), (0, -1500))
-        writer.write_pin(buf, pin)
-        self.assertEqual(buf.getvalue(), 'X ~ 2 0 -1500 200 U 60 60 1 1 B\n')
+        line = writer.get_pin_line(pin)
+        self.assertEqual(
+            line, 'X ~ 2 0 -1500 200 U 60 60 %(unit)d %(convert)d B\n')
 
-        buf = StringIO()
         pin = Pin('2', (0, 1300), (0, 1500))
-        writer.write_pin(buf, pin)
-        self.assertEqual(buf.getvalue(), 'X ~ 2 0 1500 200 D 60 60 1 1 B\n')
+        line = writer.get_pin_line(pin)
+        self.assertEqual(
+            line, 'X ~ 2 0 1500 200 D 60 60 %(unit)d %(convert)d B\n')
 
     def test_write_library_footer(self):
         writer = KiCAD()
