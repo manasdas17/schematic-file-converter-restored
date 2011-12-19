0c38b23873e40389c495c17ea6174e3a442391ef
Sebastian Vetter <sebastian@roadside-developer.com>
2011-12-19 10:16:02 -0700

    implemented writing project structure in gEDA writer

---
+++ test/geda/test_geda_writer.py
@@ -0,0 +1,36 @@
+﻿
+import os
+import unittest
+import tempfile
+
+from writer.geda import GEDA
+
+class TestGEDAWriter(unittest.TestCase):
+
+    def setUp(self):
+        self.geda_writer = GEDA()
+
+    def test_create_project_files(self):
+        geda_filename = '/tmp/test_geda.sch'
+
+        self.geda_writer.create_project_files(geda_filename)
+
+        self.assertEquals(
+            self.geda_writer.project_dirs['project'], 
+            '/tmp'
+        )
+        self.assertEquals(
+            self.geda_writer.project_dirs['symbol'], 
+            '/tmp/symbols'
+        )
+
+        self.assertTrue(os.path.exists('/tmp/gafrc'))
+
+        fh = open('/tmp/gafrc', 'r')
+        data = ''.join(fh.readlines())
+        fh.close()
+
+        self.assertEquals(data, '(component-library "./symbols")') 
+
+
+         
--- writer/geda.py
+++ writer/geda.py
@@ -1,19 +1,47 @@
 #!/usr/bin/env python
 #
 # Basic Strategy
-# 0) ...
+# 0) converted file will be store in subdirectory [TODO]
+# 1) create subdirectory, symbol and project file [TODO]
+# 2) Write each component into a .sym file [TODO]
+# 3) Write component instances to .sch file [TODO]
+# 4) Store net segments at the end of .sch file [TODO]
+
+import os
 
 
 class GEDA:
     """ The gEDA Format Writer """
 
     def __init__(self):
-        pass
+        self.offset = None
 
+        self.project_dirs = {
+            'symbol': None,
+            'project': None,
+        }
 
     def write(self, design, filename):
         """ Write the design to the gEDA format """
+
         f = open(filename, "w")
         # Write!
         f.close()
         return
+
+    def create_project_files(self, filename):
+        project_dir = os.path.dirname(filename)
+        symbol_dir = os.path.join(project_dir, 'symbols')
+
+        if not os.path.exists(symbol_dir):
+            os.mkdir(symbol_dir)
+
+        ## create project file to allow gEDA find symbol files
+        project_file = 'gafrc'
+        fh = open(os.path.join(project_dir, 'gafrc'), 'w')
+        fh.write('(component-library "./symbols")')
+        fh.close()
+
+        self.project_dirs['symbol'] = symbol_dir
+        self.project_dirs['project'] = project_dir 
+
