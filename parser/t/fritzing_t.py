38026d2028df518de4337034ee5d51c986932dfc
Dave Peticolas <dave@krondo.com>
2012-01-09 07:52:00 -0800

    Fritzing parser tests.

--- parser/t/fritzing_t.py
+++ parser/t/fritzing_t.py
@@ -21,13 +21,71 @@
 
 
 from parser.fritzing import Fritzing
-import unittest
+from parser.fritzing import make_x, make_y, make_length
+from parser.fritzing import get_x, get_y, get_length
+from unittest import TestCase
+
+from os.path import dirname, join
+
+TEST_DIR = join(dirname(__file__), '..', '..', 'test', 'fritzing')
 
 
-class FritzingTests(unittest.TestCase):
+class FritzingTests(TestCase):
     """ The tests of the fritzing parser """
+
+    def load_file(self, basename):
+        """ Load a fritzing test file with the given basename """
+        parser = Fritzing()
+        return parser.parse(join(TEST_DIR, basename))
 
     def test_create_new_fritzing_parser(self):
         """ Test creating an empty parser. """
         parser = Fritzing()
         assert parser != None
+
+    def test_make_x(self):
+        """ make_x converts x values correctly """
+        self.assertEqual(make_x('17.23'), 172)
+
+    def test_make_y(self):
+        """ make_y converts y values correctly """
+        self.assertEqual(make_y('17.23'), -172)
+
+    def test_make_length(self):
+        """ make_length converts other numeric values correctly """
+        self.assertEqual(make_length('17.23'), 172)
+
+    def test_get_x(self):
+        """ get_x retrieves x values correctly """
+        elem = {'x': '4.42', 'x1': '10.2'}
+        self.assertEqual(get_x(elem), 44)
+        self.assertEqual(get_x(elem, 'x1'), 102)
+        self.assertEqual(get_x(elem, 'x2'), 0)
+
+    def test_get_y(self):
+        """ get_y retrieves y values correctly """
+        elem = {'y': '4.42', 'y1': '10.2'}
+        self.assertEqual(get_y(elem), -44)
+        self.assertEqual(get_y(elem, 'y1'), -102)
+        self.assertEqual(get_y(elem, 'y2'), 0)
+
+    def test_get_length(self):
+        """ get_length retrieves other numeric values correctly """
+        elem = {'r': '4.42', 'r1': '10.2'}
+        self.assertEqual(get_length(elem, 'r'), 44)
+        self.assertEqual(get_length(elem, 'r1'), 102)
+        self.assertEqual(get_length(elem, 'r2'), 0)
+
+    def test_components(self):
+        """ The parser loads Components correctly """
+        design = self.load_file('components.fz')
+        self.assertEqual(len(design.components.components), 2)
+        cpts = design.components.components
+        self.assertEqual(set(cpts),
+                         set(['ResistorModuleID',
+                              '4a300fed-afa9-4e78-a643-ec209be7e3b8']))
+        diode = cpts['4a300fed-afa9-4e78-a643-ec209be7e3b8']
+        self.assertEqual(diode.name, '4a300fed-afa9-4e78-a643-ec209be7e3b8')
+        self.assertEqual(diode.attributes, {'_prefix': 'D'})
+        self.assertEqual(len(diode.symbols), 1)
+
