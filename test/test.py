d3db26229a8087a2cd5119f767c4a91241d30eb0
Dave Peticolas <dave@krondo.com>
2012-05-15 20:59:18 -0700

    Rework diff test to use diff program.

--- test/test.py
+++ test/test.py
@@ -27,8 +27,9 @@ import os
 import sys
 import re
 import logging
+import subprocess
 import tempfile
-from difflib import SequenceMatcher
+
 from upconvert.upconverter import Upconverter
 
 
@@ -69,21 +70,27 @@ def filter_upv(arg, top, names):
             arg.append(os.path.join(top, name))
 
 
-def file_diff(file1, file2):
-    text1 = ''
+def get_file_diff_ratio(file1, file2):
+    """ Return the ratio of differences to the total
+    number of lines in file1 and file2. """
     with open(file1) as f:
-        text1 = f.read()
-    text2 = ''
+        num_lines1 = sum(1 for _ in f)
     with open(file2) as f:
-        text2 = f.read()
-    return SequenceMatcher(None, text1, text2)
+        num_lines2 = sum(1 for _ in f)
+
+    p = subprocess.Popen(('diff', '--speed-large-files', file1, file2),
+                         stdout=subprocess.PIPE)
+
+    num_diffs = sum(1 for line in p.stdout if line and line[0] in '<>')
+
+    p.wait()
+
+    return float(num_diffs) / (num_lines1 + num_lines2)
 
 
-def check_output(tmp_path):
-    output = None
+def get_file_contents(tmp_path):
     with open(tmp_path, 'r') as final:
-        output = final.read()
-    return output
+        return final.read()
 
 
 def test_auto_detect_generator(file_path, format):
@@ -102,25 +109,58 @@ def test_parse_generator(file_path, format):
         tmp_fd, tmp_path = tempfile.mkstemp()
         os.close(tmp_fd)
         Upconverter.write(data, tmp_path, 'openjson')
-        self.assertTrue(check_output(tmp_path) != '')
+        self.assertTrue(get_file_contents(tmp_path) != '')
         os.remove(tmp_path)
     return test
 
 
 def test_diff_generator(file_path, format):
-    """ Test the damage to a file from an in-out. """
+    """ Parse the file, write to both openjson and to the given
+    format. Then parse the new file in the same format and write that
+    to openjson. Test the differences between the two openjson files
+    and fail if they differ by more than 5 percent.
+
+    This tests that the parser and writer together preserve as much
+    information as the parser by itself.
+
+    Given a file F in format X, written as F.X, do the following
+    conversions:
+
+      F.X --> F1.upv --> F2.X --> F3.upv
+
+    Then compare F1 and F3 using the 'diff' program and compute the
+    ratio of the number of differences to the total number of lines in
+    both files. Assert the ratio is below a given threshold.
+    """
     def test(self):
         data = Upconverter.parse(file_path, format)
-        self.assertTrue(data != None)
+        self.assertNotEqual(data, None)
 
-        tmp_fd, tmp_path = tempfile.mkstemp()
+        tmp_fd, json_path_1 = tempfile.mkstemp(prefix='test.1.', suffix='.upv')
+        os.close(tmp_fd)
+        Upconverter.write(data, json_path_1, 'openjson')
+        self.assertNotEqual(get_file_contents(json_path_1), '')
+
+        tmp_fd, tmp_path = tempfile.mkstemp(suffix='.' + format)
         os.close(tmp_fd)
         Upconverter.write(data, tmp_path, format)
-        self.assertTrue(check_output(tmp_path) != '')
+        self.assertNotEqual(get_file_contents(tmp_path), '')
 
-        ratio = file_diff(file_path, tmp_path).ratio()
-        self.assertTrue(ratio > 0.95,
-                        (file_path, tmp_path, ratio))
+        data = Upconverter.parse(tmp_path, format)
+        self.assertNotEqual(data, None)
+
+        tmp_fd, json_path_2 = tempfile.mkstemp(prefix='test.2.', suffix='.upv')
+        os.close(tmp_fd)
+        Upconverter.write(data, json_path_2, 'openjson')
+        self.assertNotEqual(get_file_contents(json_path_2), '')
+
+        ratio = get_file_diff_ratio(json_path_1, json_path_2)
+
+        self.assertTrue(ratio < 0.05,
+                        (file_path, tmp_path, json_path_1, json_path_2, ratio))
+
+        os.remove(json_path_1)
+        os.remove(json_path_2)
         os.remove(tmp_path)
     return test
 
@@ -134,7 +174,7 @@ def test_write_generator(json_file_path, format):
         tmp_fd, tmp_path = tempfile.mkstemp()
         os.close(tmp_fd)
         Upconverter.write(data, tmp_path, format)
-        self.assertTrue(check_output(tmp_path) != '')
+        self.assertTrue(get_file_contents(tmp_path) != '')
         os.remove(tmp_path)
     return test
 
@@ -209,11 +249,10 @@ def main():
             test = test_parse_generator(f, format)
             setattr(test_class, test_name, test)
 
-            # # TODO: There is a bug that causes diff to hang on openjson
-            # if format not in ('fritzing', 'openjson', 'gerber'):
-            #     test_name = 'test_%s_%s_%s' % (format, base, 'diff')
-            #     test = test_diff_generator(f, format)
-            #     setattr(test_class, test_name, test)
+            if format not in ('fritzing', 'gerber'):
+                test_name = 'test_%s_%s_%s' % (format, base, 'diff')
+                test = test_diff_generator(f, format)
+                setattr(test_class, test_name, test)
 
         for f in upverter_upv_files:
             base = os.path.basename(f)
