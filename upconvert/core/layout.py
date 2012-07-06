fd2a93589aa537f261ab01d44e15a38d66217c4d
Zak Homuth <zak@upverter.com>
2012-07-06 15:27:50 -0400

    bug

--- upconvert/core/layout.py
+++ upconvert/core/layout.py
@@ -19,7 +19,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from upconvert.utils.shape import Arc
+
+from upconvert.core.shape import Arc
 
 
 class Layout:
