2cc96e7c121282f3eec6fec0f7dfc17c28c07d7e
Stephen Hamer <stephen.hamer@gmail.com>
2013-01-28 11:51:48 -0500

    rev version to v0.8.3

--- upconvert/version.py
+++ upconvert/version.py
@@ -30,6 +30,6 @@ def version():
         with open('version', 'r') as f:
             vrsn = f.read().strip()
     except Exception: #pylint: disable=W0703
-        vrsn = '0.8.2'
+        vrsn = '0.8.3'
 
     return vrsn
